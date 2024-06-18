8633028ec0c75e368ae499d8b7b315fd8aca64b1
Dodong2 <carlseighartaliodegmail.com>
2024-06-18 11:29:05 +0800

    third commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,30 +12,30 @@ import { RiStackshareFill } from "react-icons/ri";
 /*---Hooks---*/
 
 const Sidebar = () => {
-    const [ isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-    <div className='sidebar-container'>
-      <aside className={`sidebar ${isOpen ? "open" : ""}`}>
-        <div className="inner">
-          <header>
-            <button
-              type="button"
-              className="sidebar-burger"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <span className="icon">{isOpen ? <IoClose /> : <RiMenu4Fill />}</span>
-            </button>
-          </header>
-          <nav className={isOpen ? "" : "notxt"}>
-          <button ><TbHomeFilled /><span>Home</span></button>
-          <button className=""><GiSkills/> <span>Settings</span></button>
-          <button><LiaProjectDiagramSolid/> <span>Build</span></button>
-          <button><FaSquarePhone/><span>Contact</span></button>
-          <button className="btn5"><RiStackshareFill/><span>Share</span></button>
-          </nav>
-        </div>
-      </aside>
+      <div className='sidebar-container'>
+        <aside className={`sidebar ${isOpen ? "open" : ""}`}>
+          <div className="inner">
+            <header>
+              <button
+                type="button"
+                className="sidebar-burger"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <span className="icon">{isOpen ? <IoClose /> : <RiMenu4Fill />}</span>
+              </button>
+            </header>
+            <nav className={isOpen ? "" : "notxt"}>
+              <button ><TbHomeFilled /><span>Home</span></button>
+              <button className=""><GiSkills /> <span>Settings</span></button>
+              <button><LiaProjectDiagramSolid /> <span>Build</span></button>
+              <button><FaSquarePhone /><span>Contact</span></button>
+              <button className="btn5"><RiStackshareFill /><span>Share</span></button>
+            </nav>
+          </div>
+        </aside>
       </div>
     </>
   )
