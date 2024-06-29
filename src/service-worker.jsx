95df4a2d6163dafeadc3319bb15dc1554d6ef6d6
Dodong2 <carlseighartaliodegmail.com>
2024-06-29 15:02:09 +0800

    23th commit

---
+++ src/service-worker.jsx
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('my-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/favicon.ico',
+          '/manifest.webmanifest',
+          '/pwa-192x192.png',
+          '/pwa-512x512.png',
+          // Add other assets here
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
