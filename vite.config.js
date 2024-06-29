942866fe8f373137157c6d3f45918e396dfb9826
Dodong2 <carlseighartaliodegmail.com>
2024-06-29 20:58:36 +0800

    42 commit

--- vite.config.js
+++ vite.config.js
@@ -15,29 +15,122 @@ const manifestForPlugin = {
     description: 'I am a simple vite app',
     icons: [
       {
-        src: '/public/android-chrome-192x192.png',
+        src: 'img1.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img2.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img3.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img4.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img5.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img6.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img7.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img8.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img9.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img10.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img11.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img12.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img13.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'img14.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: 'appcon.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'favicon',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: 'bao.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'favicon',
+        
       },
       {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-      {
-        src: '/maskable_icon.png',
+        src: 'budget tracker.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
-      },
+        
+      },{
+        src: 'catfact.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      },{
+        src: 'cert-AppCon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      },{
+        src: 'dong.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      },{
+        src: 'php crud.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      },{
+        src: 'port design.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      },{
+        src: 'weather_app.png',
+        sizes: '512x512',
+        type: 'image/png',
+        
+      }
     ],
     theme_color: '#181818',
     background_color: '#e0cc3b',
