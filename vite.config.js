fbc5d6d3206c7de678f89a9d84a93847074b1637
Dodong2 <carlseighartaliodegmail.com>
2024-06-29 21:03:35 +0800

    43 commit

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,34 @@ const manifestForPlugin = {
     'favicon.ico',           // Include favicon
     'apple-touch-icon.png',  // Include apple touch icon
     'masked-icon.svg',       // Include masked icon
+    '/appcon.png',
+  '/bao.png',
+  '/budget tracker.png',
+  '/catfact.png',
+  '/cert-AppCon.png',
+  '/dong.png',
+  '/php crud.png',
+  '/port design.png',
+  '/weather_app.png',
+  '/img1.svg',
+  '/img2.svg',
+  '/img3.svg',
+  '/img4.svg',
+  '/img5.svg',
+  '/img6.svg',
+  '/img7.svg',
+  '/img8.svg',
+  '/img9.svg',
+  '/img10.svg',
+  '/img11.svg',
+  '/img12.svg',
+  '/img13.svg',
+  '/img14.svg',
   ],
   manifest: {
-    name: 'React-vite-app',
-    short_name: 'react-vite-app',
-    description: 'I am a simple vite app',
+    name: 'Castar App',
+    short_name: 'Castar Web App',
+    description: 'Castar App Portfolio',
     icons: [
       {
         src: 'img1.svg',
