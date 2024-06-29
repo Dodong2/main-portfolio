23fd53d1c087bf00136569b95795619d4760e5cd
Dodong2 <carlseighartaliodegmail.com>
2024-06-29 18:31:13 +0800

    30th commit

--- vite.config.js
+++ vite.config.js
@@ -16,23 +16,124 @@ export default defineConfig({
         'icons/*.svg'  // Include all SVG files in the icons directory
       ],
       manifest: {
-        name: 'My Awesome App',
+        name: 'Castar Portfolio App',
         short_name: 'MyApp',
         description: 'My Awesome App description',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'icons/pwa-icon.svg',
+            src: 'img1.svg',
             sizes: 'any',
             type: 'image/svg+xml',
           },
           {
-            src: 'icons/pwa-192x192.png',
+            src: 'img2.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img3.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img4.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img5.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img6.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img7.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img8.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img9.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img10.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img11.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img12.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img13.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'img14.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+          {
+            src: 'appcon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'icons/pwa-512x512.png',
+            src: 'bao.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'budget tracker.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'catfact.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'cert-AppCon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'dong.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'php crud.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'port design.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },{
+            src: 'weather_app.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
